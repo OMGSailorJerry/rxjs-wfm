70d99bea9d3d32c29a35df8504d4602e5c81b597
Oleg Mikhailov <omikhailov@ace-st.com>
2018-03-22 12:57:55 +0200

    Get user by ID from V

--- src/app.js
+++ src/app.js
@@ -2,17 +2,33 @@
 
 import Rx from 'rxjs/Rx';
 
-function createSubscription(name) {
-    return {
-        next(x) {
-            console.log(`${name}:`, x);
-        },
-        error(err) {
-            console.error(`${name}: ${err}`)
-        },
-        complete() {
-            console.log(`${name}: Completed!`)
-        }
-    }    
+function getUserById(id) {
+    const params = {
+        access_token: '27434b9a8551eaff383667ee07dfd0ab3cc1efc35a9cb788e9b1780ff70d9d4f240279d337e45a88887a0',
+        user_ids: id,
+        fields: 'photo_big',
+        v: '5.52'
+    };
+
+    return $.ajax({
+        url: 'https://api.vk.com/method/users.get?' + $.param(params),
+        type: 'GET',
+        dataType: 'JSONP'
+    }).promise()
 };
 
+Rx.Observable.fromEvent($('input'), 'keyup')
+    .pluck('target', 'value')
+    .distinct()
+    .debounceTime(2000)
+    .mergeMap(res => Rx.Observable.fromPromise(getUserById(res)))
+    .catch(error => Rx.Observable.of(error))
+    .map(res => res.response[0])
+    .subscribe(
+        res => {
+            $('h1').html(`${res.first_name} ${res.last_name} <i>${res.id}</i>`)
+            $('img').attr('src', res.photo_big);
+        },
+        err => console.error(err),
+        () => console.log('Completed')
+    );
